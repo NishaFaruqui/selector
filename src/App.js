048ac9778d35658f619e3616c37f36a4dea65070
Nisha <nizfaruqui512@gmail.com>
2019-04-17 20:42:42 -0400

    added component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-class App extends Component {
+import React, { Component } from 'react';
+
+
+class Selector extends Component {
   render() {
     return (
-
+    <div className="container">
+      <div className="navbar">
+        <div>Selected:</div>
+        <div>What goes here?</div>
+      </div>
+      <div className="shape-list">
+        Add the Shape Components here!
+      </div>
+    </div>
     );
   }
 }
 
-export default App;
+export default Selector;
